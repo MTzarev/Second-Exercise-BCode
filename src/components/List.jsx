1ca6a3d82ad0f200d9157a5f15ae117cfef19d80
Miroslav Tzarev <mtzarev@gmail.com>
2025-01-22 16:28:12 +0200

    how to render an array with objects

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,19 @@
 function List () {
     
-    const fruits = [ "orange","apple", "banana", "coconat", "pineapple" ]
-   let sorted = fruits.sort()
-   const listItems = sorted.map(x=><li>{x}</li>)
-   
-    return( 
+    const fruits = [    {name: "orange", calories: 45},
+                        {name: "apple", calories: 95}, 
+                        {name: "banana", calories: 105}, 
+                        {name: "coconat", calories: 159}, 
+                        {name: "pineapple", calories:37} 
+                    ]
+//let listItems = fruits.map(fruit=> <li>{fruit}</li>)
+    //    let sorted = fruits.sort()
+//    const listItems = sorted.map(x=><li>{x}</li>)
+   // ! when we wont to render an aray with objects:
+ 
+   const listItems = fruits.map((x)=><li key={x.name}>{x.name} - calories:{x.calories}</li>)
+    
+   return( 
         <ol>{listItems}</ol>
     );
 }

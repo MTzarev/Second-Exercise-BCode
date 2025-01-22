a4268f8c31a40d3d70c525910faddef77fe10cbd
Miroslav Tzarev <mtzarev@gmail.com>
2025-01-22 17:24:00 +0200

    make ListComponent reusable

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,28 @@ import UserGreeting from './components/UserGreeting'
 import List from './components/List'
 
 function App() {
+
+      
+  const fruits = [ {id:1, name: "orange", calories: 45},
+                   {id:2, name: "apple", calories: 95}, 
+                   {id:3, name: "banana", calories: 105}, 
+                   {id:4, name: "coconat", calories: 159}, 
+                   {id:5, name: "pineapple", calories:37}];
+
+   const vegetables = [ {id:1, name: "potatoes", calories: 110},
+                    {id:2, name: "celery", calories: 15}, 
+                    {id:3, name: "carrots", calories: 25}, 
+                    {id:4, name: "corn", calories: 63}, 
+                    {id:5, name: "broccoli", calories:50}];
   return (
     <>
-    <List/>
+    <List items={fruits} category="Fruits"/>
+    <List items={vegetables} category="Vegetables"/>
+
+
+
+
+
     <UserGreeting isLoggedIn = {true} />
     </>
   )
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,25 +1,17 @@
-function List () {
-    
-    const fruits = [    {id:1, name: "orange", calories: 45},
-                        {id:2, name: "apple", calories: 95}, 
-                        {id:3, name: "banana", calories: 105}, 
-                        {id:4, name: "coconat", calories: 159}, 
-                        {id:5, name: "pineapple", calories:37} 
-                    ]
-//let listItems = fruits.map(fruit=> <li>{fruit}</li>)
-    //    let sorted = fruits.sort()
-//    const listItems = sorted.map(x=><li>{x}</li>)
-   // ! when we wont to render an aray with objects:
+function List (props) {
 
-   //fruits.sort ((a,b)=>a.name.localeCompare(b.name))
-fruits.sort((a,b)=>b.calories-a.calories)
-   const listItems = fruits.map((x)=><li key={x.id}>
-    {x.name} &nbsp; <b>calories:
-    {x.calories}</b></li>
-    );
+    const category = props.category;
+    const itemList = props.items;
+
+const listItems = itemList.map((x)=><li key={x.id}>
+                            {x.name} &nbsp; <b>calories:
+                            {x.calories}</b></li>);
     
    return( 
+    <>
+        <h1>{category}</h1>
         <ol>{listItems}</ol>
+    </>
     );
 }
 export default List;

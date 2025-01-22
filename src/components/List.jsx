37e978affd2667a68d83d717d3067d506c4be798
Miroslav Tzarev <mtzarev@gmail.com>
2025-01-22 16:40:36 +0200

    insert sort method - sort Alphabetical

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,17 +1,22 @@
 function List () {
     
-    const fruits = [    {name: "orange", calories: 45},
-                        {name: "apple", calories: 95}, 
-                        {name: "banana", calories: 105}, 
-                        {name: "coconat", calories: 159}, 
-                        {name: "pineapple", calories:37} 
+    const fruits = [    {id:1, name: "orange", calories: 45},
+                        {id:2, name: "apple", calories: 95}, 
+                        {id:3, name: "banana", calories: 105}, 
+                        {id:4, name: "coconat", calories: 159}, 
+                        {id:5, name: "pineapple", calories:37} 
                     ]
 //let listItems = fruits.map(fruit=> <li>{fruit}</li>)
     //    let sorted = fruits.sort()
 //    const listItems = sorted.map(x=><li>{x}</li>)
    // ! when we wont to render an aray with objects:
- 
-   const listItems = fruits.map((x)=><li key={x.name}>{x.name} - calories:{x.calories}</li>)
+
+   fruits.sort ((a,b)=>a.name.localeCompare(b.name))
+
+   const listItems = fruits.map((x)=><li key={x.id}>
+    {x.name} &nbsp; <b>calories:
+    {x.calories}</b></li>
+    );
     
    return( 
         <ol>{listItems}</ol>
